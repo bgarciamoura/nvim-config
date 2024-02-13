3a8a0e7405712377014a039c670dfe0c9bfc9cd2
Bruno Garcia Moura <bgarciamoura@gmail.com>
2024-02-13 16:44:41 -0300

    feat: New config on lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,4 +1,68 @@
 local lsp = require("lspconfig");
+
+-- LSPs
+lsp.tsserver.setup({
+	settings = {
+		-- TypeScript plugin settings
+		analysis = {
+			extraPaths = {
+				"./node_modules",
+				"./src",
+				"/Volumes/SSD500GB/npm-global",
+				"/Volumes/SSD500GB/npm-global/lib/node_modules",
+				"/Volumes/SSD500GB/npm-global/lib/node_modules/typescript",
+				"/Volumes/SSD500GB/Projects/dio-blog/node_modules",
+			},
+		}
+	}
+});
+
+lsp.cssls.setup({
+	settings = {
+		-- CSS plugin settings
+		less = {
+			validate = true
+		},
+		scss = {
+			validate = true
+		},
+		sass = {
+			validate = true
+		},
+	},
+});
+
+lsp.html.setup({
+	settings = {
+		-- HTML plugin settings
+		html = {
+			validate = true
+		}
+	}
+});
+
+lsp.angularls.setup({
+	settings = {
+		-- Angular plugin settings
+		analytics = {
+			lsAngular = {
+				ng = {
+				},
+				extraPaths = {
+					"./node_modules",
+					"./src",
+					"/Volumes/SSD500GB/npm-global",
+					"/Volumes/SSD500GB/npm-global/lib/node_modules",
+					"/Volumes/SSD500GB/npm-global/lib/node_modules/typescript",
+					"/Volumes/SSD500GB/Projects/dio-blog/node_modules",
+				}
+			}
+		},
+		angular = {
+		}
+	}
+});
+
 -- local coq = require("coq");
 
 -- lsp.tsserver.setup(coq.lsp_ensure_capabilities());
